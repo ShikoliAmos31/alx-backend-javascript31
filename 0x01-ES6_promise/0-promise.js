89bbb70dc63d154a00c7099e3bc1bd210021a795
shikoliamos31 <amosshikoli@gmail.com>
2024-06-27 00:18:58 +0300

    Await / Async

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,3 @@
-function getFullResponseFromAPI(apiEndpoint) {
-  return new Promise((resolve, reject) => {
-    if (apiEndpoint) resolve({ status: 200, body: 'Success' });
-    reject(Error('Failed to fetch API'));
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-export default getFullResponseFromAPI;
